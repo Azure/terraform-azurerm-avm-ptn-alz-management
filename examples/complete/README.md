735c3066904efc200115d8dfc44c02d2f9a998a6
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-12-17 17:01:33 +0000

    fix: make outputs sensitive (#75)

--- examples/complete/README.md
+++ examples/complete/README.md
@@ -3,7 +3,8 @@
 
 This shows how to use the variables to customise the management resources for Azure Landing Zones.
 
-> [!WARNING] The Log Analytics SKU in this example is `CapacityReservation`, which will result in additional charges. Please change the SKU to PerGB2018 to avoid these charges.
+> [!WARNING]
+> The Log Analytics SKU in this example is `CapacityReservation`, which will result in significant charges once data is collected. Please change the SKU to PerGB2018 to avoid these charges.
 
 ```hcl
 resource "random_id" "id" {
--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,23 @@
 output "automation_account" {
   description = "A curated output of the Azure Automation Account."
   value = {
-    dsc_primary_access_key   = try(azurerm_automation_account.management[0].dsc_primary_access_key, null)
-    dsc_secondary_access_key = try(azurerm_automation_account.management[0].dsc_secondary_access_key, null)
-    dsc_server_endpoint      = try(azurerm_automation_account.management[0].dsc_server_endpoint, null)
-    hybrid_service_url       = try(azurerm_automation_account.management[0].hybrid_service_url, null)
-    id                       = try(azurerm_automation_account.management[0].id, null)
-    name                     = try(azurerm_automation_account.management[0].name, null)
+    dsc_server_endpoint = try(azurerm_automation_account.management[0].dsc_server_endpoint, null)
+    hybrid_service_url  = try(azurerm_automation_account.management[0].hybrid_service_url, null)
+    id                  = try(azurerm_automation_account.management[0].id, null)
+    name                = try(azurerm_automation_account.management[0].name, null)
     identity = {
       tenant_id    = try(azurerm_automation_account.management[0].identity[0].tenant_id, null)
       principal_id = try(azurerm_automation_account.management[0].identity[0].principal_id, null)
     }
+  }
+}
+
+output "automation_account_dsc_keys" {
+  description = "Sensitive values for the Azure Automation Account."
+  sensitive   = true
+  value = {
+    primary   = try(azurerm_automation_account.management[0].dsc_primary_access_key, null)
+    secondary = try(azurerm_automation_account.management[0].dsc_secondary_access_key, null)
   }
 }
 
@@ -25,11 +32,18 @@ output "data_collection_rule_ids" {
 output "log_analytics_workspace" {
   description = "A curated output of the Log Analytics Workspace."
   value = {
-    id                   = azurerm_log_analytics_workspace.management.id
-    name                 = azurerm_log_analytics_workspace.management.name
-    primary_shared_key   = azurerm_log_analytics_workspace.management.primary_shared_key
-    secondary_shared_key = azurerm_log_analytics_workspace.management.secondary_shared_key
-    workspace_id         = azurerm_log_analytics_workspace.management.workspace_id
+    id           = azurerm_log_analytics_workspace.management.id
+    name         = azurerm_log_analytics_workspace.management.name
+    workspace_id = azurerm_log_analytics_workspace.management.workspace_id
+  }
+}
+
+output "log_analytics_workspace_keys" {
+  description = "Sensitive values for the Log Analytics Workspace."
+  sensitive   = true
+  value = {
+    primary   = azurerm_log_analytics_workspace.management.primary_shared_key
+    secondary = azurerm_log_analytics_workspace.management.secondary_shared_key
   }
 }
 
