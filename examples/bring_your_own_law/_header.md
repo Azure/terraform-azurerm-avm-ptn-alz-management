d66d70dc3ac10c745ee220c77b62d37b99117dee
nathan bird <nathan.bird@servent.co.uk>
2025-07-22 23:24:54 +0100

    feat: add example configuration for BYO LAW

---
+++ examples/bring_your_own_law/_header.md
@@ -0,0 +1,3 @@
+# Basic of the Azure Landings Zones Management Module
+
+This code sample shows how to create the resources using default settings.
---
+++ examples/bring_your_own_law/main.tf
@@ -0,0 +1,14 @@
+resource "random_id" "id" {
+  byte_length = 4
+}
+
+module "management" {
+  source = "../.."
+
+  automation_account_name                  = "aa-terraform-${random_id.id.hex}"
+  location                                 = "eastus"
+  log_analytics_workspace_creation_enabled = false
+  log_analytics_workspace_id               = "<EXISTING_LOG_ANALYTICS_WORKSPACE_ID>"
+  resource_group_creation_enabled          = false
+  resource_group_name                      = "<EXISTING_RESOURCE_GROUP_NAME>"
+}
