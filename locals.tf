1139dc42271cfca7283a9d644781b0aaa203663c
nathan bird <nathan.bird@servent.co.uk>
2025-07-22 23:24:26 +0100

    fix: ensure log analytics workspace name is set correctly in locals

--- locals.tf
+++ locals.tf
@@ -1,4 +1,5 @@
 locals {
-  resource_group_name        = var.resource_group_creation_enabled ? azurerm_resource_group.management[0].name : var.resource_group_name
-  resource_group_resource_id = var.resource_group_creation_enabled ? azurerm_resource_group.management[0].id : provider::azapi::build_resource_id("/subscriptions/${data.azapi_client_config.current.subscription_id}", "Microsoft.Resources/resourceGroups", var.resource_group_name)
+  resource_group_name          = var.resource_group_creation_enabled ? azurerm_resource_group.management[0].name : var.resource_group_name
+  resource_group_resource_id   = var.resource_group_creation_enabled ? azurerm_resource_group.management[0].id : provider::azapi::build_resource_id("/subscriptions/${data.azapi_client_config.current.subscription_id}", "Microsoft.Resources/resourceGroups", var.resource_group_name)
+  log_analytics_workspace_name = var.log_analytics_workspace_name != null ? var.log_analytics_workspace_name : "law-prod-${var.location}-001"
 }
